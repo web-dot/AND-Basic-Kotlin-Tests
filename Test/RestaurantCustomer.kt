504681afb773c7af9e01212465a95845deeb7275
sudipto <cg.sudipc@gmail.com>
2023-10-11 01:04:11 +0530

    test

--- Test/RestaurantCustomer.kt
+++ Test/RestaurantCustomer.kt
@@ -1,6 +1,23 @@
 
+abstract class Person(val name: String){
+   abstract fun greet()
+}
+
+interface FoodConsumer{
+   fun eat()
+   fun pay(amount: Int) = println("Delicious! Here's ${amount} bucks!")
+}
+
+class RestaurantCustomer(name: String, val dish: String) : Person(name), FoodConsumer{
+   fun order() =  println("$dish, please")
+   override fun eat() = println("*Eats $dish*")
+   override fun greet() = println("Its me, $name") 
+}
 
 fun main(){
-	
-	println("working")
+	val sam = RestaurantCustomer("Sam", "Mixed Salad")
+   sam.greet() // An inplementation of an abstract function
+   sam.order() // A member function
+   sam.eat()   // An implementation of an interface function
+   sam.pay(10) // A default implementation in an interface
 }
